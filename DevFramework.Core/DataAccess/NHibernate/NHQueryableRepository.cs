a2b8a673a54ea4340e11a03c650f7fb8e09a9254
Furkan Karataş <furkan.karatas1414@gmail.com>
2020-05-31 16:49:41 +0300

    Add project files.

---
+++ DevFramework.Core/DataAccess/NHibernate/NHQueryableRepository.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DevFramework.Core.Entities;
+
+namespace DevFramework.Core.DataAccess.NHibernate
+{
+    class NHQueryableRepository<T>:IQueryableRepository<T> where T:class,IEntity,new()
+    {
+        private NHibernateHelper _nHibernateHelper;
+        private IQueryable<T> _entities;
+        public NHQueryableRepository(NHibernateHelper nHibernateHelper)
+        {
+            _nHibernateHelper = nHibernateHelper;
+        }
+
+
+        public IQueryable<T> Table
+        {
+            get { return this.Entites; }
+        }
+
+        public virtual IQueryable<T> Entites
+        {
+            get { return _entities ?? (_entities = _nHibernateHelper.OpenSession().Query<T>()); }
+        }
+    }
+}
