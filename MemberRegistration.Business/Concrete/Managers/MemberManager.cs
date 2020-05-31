44f4da2f08689c4b1cbe031178262c705526a096
Furkan Karataş <furkan.karatas1414@gmail.com>
2020-05-31 17:40:13 +0300

    Add MemberService and manager

---
+++ MemberRegistration.Business/Concrete/Managers/MemberManager.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MemberRegistration.Business.Abstract;
+using MemberRegistration.DataAccess.Abstract;
+using MemberRegistration.Entities.Concrete;
+
+namespace MemberRegistration.Business.Concrete.Managers
+{
+    public class MemberManager:IMemberService
+    {
+        private IMemberDal _memberDal;
+
+        public MemberManager(IMemberDal memberDal)
+        {
+            _memberDal = memberDal;
+        }
+
+        public void Add(Member member)
+        {
+            _memberDal.Add(member);
+        }
+    }
+}
